2e285d4d5e33b9e0ff2ed37ee3cc87efe160c91e
KLAC8 <ahusan.kelaalac@gmail.com>
2025-07-09 10:12:41 +0500

    category [id] route.ts file updated

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -2,21 +2,31 @@ import { NextResponse } from "next/server";
 import clientPromise from "../../../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request) {
+  const url = new URL(request.url);
+  const id = url.pathname.split("/").pop();
+
+  if (!id) return new NextResponse("Missing id", { status: 400 });
+
   const { name } = await request.json();
   if (!name) return new NextResponse("Missing category name", { status: 400 });
 
   const client = await clientPromise;
   const db = client.db();
 
-  await db.collection("categories").updateOne({ _id: new ObjectId(params.id) }, { $set: { name } });
-  return NextResponse.json({ _id: params.id, name });
+  await db.collection("categories").updateOne({ _id: new ObjectId(id) }, { $set: { name } });
+  return NextResponse.json({ _id: id, name });
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request) {
+  const url = new URL(request.url);
+  const id = url.pathname.split("/").pop();
+
+  if (!id) return new NextResponse("Missing id", { status: 400 });
+
   const client = await clientPromise;
   const db = client.db();
 
-  await db.collection("categories").deleteOne({ _id: new ObjectId(params.id) });
+  await db.collection("categories").deleteOne({ _id: new ObjectId(id) });
   return new NextResponse(null, { status: 204 });
 }
