f0d30e366e65f3610ac7cc4d14109788469de745
KLAC8 <ahusan.kelaalac@gmail.com>
2025-07-09 15:54:29 +0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
-"use client";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+export const metadata = {
+  title: "Kelaa LAC Inventory",
+  description: "Inventory Management App for Kelaa Local Authority Company",
+  icons: {
+    icon: "/favicon.png", // Make sure this file exists in /public
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <ClerkProvider>{children}</ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
